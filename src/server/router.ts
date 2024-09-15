11fe22b8c86cd4e5103d6055b4f5a5ccd93e9cd1
josuerx12 <josueaze12@gmail.com>
2024-09-15 10:28:21 -0300

    :sparkles:feat: importação da rota de users no router da aplicação.

--- src/server/router.ts
+++ src/server/router.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { userRoutes } from "./routes/user.route";
 
 export const routes = Router();
 
 routes.get("/", (req, res) => {
   res.send("Hello world");
 });
+
+routes.use("/users", userRoutes);
