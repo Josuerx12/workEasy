7665e0bc940c3558ad4e0811f7a2be89910da04f
josuerx12 <josueaze12@gmail.com>
2024-11-05 18:41:31 -0300

    :hammer:refactor: Ajuste no model mapper e retorno do toJSON da entidade de user.

--- src/core/user/infra/models/user.model.mapper.ts
+++ src/core/user/infra/models/user.model.mapper.ts
@@ -1,6 +1,7 @@
 import { Prisma } from "@prisma/client";
 import { UserEntity } from "../../domain/entities/user.entity";
 import { AvatarModelMapper } from "src/core/avatar/infra/models/avatar.model.mapper";
+import { CompanyModelMapper } from "@src/core/company/infra/models/company.model.mapper";
 
 export class UserModelMapper {
   static toModel(user: UserEntity): Prisma.userCreateInput {
@@ -33,9 +34,19 @@ export class UserModelMapper {
       avatarId: model.avatarId,
       moderator: model.moderator,
       support: model.support,
-      company: model.company,
-      companyRequester: model.companyRequester,
-      companyUser: model.companyUser,
+      company: model.company
+        ? model.company?.map((company) => CompanyModelMapper.toEntity(company))
+        : null,
+      companyRequester: model.companyRequester
+        ? model.companyRequester?.map((companyRequester) =>
+            CompanyModelMapper.toEntity(companyRequester)
+          )
+        : null,
+      companyUser: model.companyUser
+        ? model.companyUser?.map((companyUser) =>
+            CompanyModelMapper.toEntity(companyUser)
+          )
+        : null,
       createdAt: model.createdAt,
       updatedAt: model.updatedAt,
       deletedAt: model.deletedAt,
